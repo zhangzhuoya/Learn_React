46cc75da63c2383b23be9e9db6dc40ddc777c4be
zhangzhuoya <1114976368@qq.com>
2021-08-17 23:15:38 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-import {createElement,render} from './SELFJSX'
-
-let jsxObj = createElement("div",{
-  className: 'box',
-  id:'box',
-  index:1
-},'珠峰培训',createElement("span",{className:'box1'},'哈哈哈哈'))
-console.log(jsxObj);
+import React  from 'react'
+import reactDom from 'react-dom'
+reactDom.render(<div></div>,document.getElementById('root'))
